eabb43d09862485a94bc933e25f3b90861cd449b
hassanq707 <qadrihassan25@gmail.com>
2025-02-14 01:54:13 +0500

    again changes

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -20,20 +20,13 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 console.log(FRONTEND_URL)
 
 
-// app.use(
-//   cors({
-//     // React app ka URL hai jahan se requests aa rahi hain.
-//     origin: FRONTEND_URL, 
-//     // Cookies ko allow karta hai ki frontend aur backend ke 
-//     // beech mein exchange ho sakein.
-//     credentials: true, 
-//   })
-// );
-
 app.use(
   cors({
-    origin: "https://emp-man-app.vercel.app", // Frontend URL
-    credentials: true,
+    // React app ka URL hai jahan se requests aa rahi hain.
+    origin: FRONTEND_URL, 
+    // Cookies ko allow karta hai ki frontend aur backend ke 
+    // beech mein exchange ho sakein.
+    credentials: true, 
   })
 );
 
